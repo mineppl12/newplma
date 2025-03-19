db7f4f31acd87810a0ff0ded3616178eda32f6c2
kim-seongchan <kimsungchan@kaist.ac.kr>
2025-03-19 22:18:06 +0900

    Update points/view, points/apply

--- src/pages/points/apply/index.jsx
+++ src/pages/points/apply/index.jsx
@@ -1,74 +1,281 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-import axios from "axios";
-import './index.scss';
+import "./index.scss";
 
-import DataTable from '~shared/ui/datatable';
+import { Card, Button, Form, InputGroup, Row, Col } from "react-bootstrap";
+import DataTable from "~shared/ui/datatable";
 
-import { Card, Button } from "react-bootstrap";
+import getData from "~shared/scripts/getData";
 
-const TITLE = import.meta.env.VITE_TITLE;
+function Points_Apply() {
+  const [columns, setColumns] = useState([]);
+  const [tableData, setTableData] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [inputs, setInputs] = useState({
+    grade: 1,
+    classNum: 1,
+    studentNum: 1,
+    reason: "",
+    plusPoints: 0,
+    minusPoints: 0,
+    act_date: "",
+    reasonCaption: "",
+  });
 
-async function getData(url, params = {}){
-    const response = await axios.get(`${url}`, {
-        params: params
+  const handleChange = (e) => {
+    const { name, value } = e.target; // name 속성 가져오기
+    setInputs((prevState) => ({
+      ...prevState,
+      [name]: value, // 동적으로 필드 업데이트
+    }));
+  };
+
+  const handleClick = (e) => {
+    const { grade, classNum, studentNum } = inputs;
+    // const stuid = `${grade}${classNum}${studentNum}`;
+  };
+
+  const handleSelect = (e) => {
+    const { value } = e.target; // name 속성 가져오기
+    const grade = parseInt(value.substring(0, 1));
+    const classNum = parseInt(value.substring(1, 2));
+    const studentNum = parseInt(value.substring(2));
+
+    setInputs({
+      grade,
+      classNum,
+      studentNum,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // 폼의 기본 제출 동작 방지
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  async function init() {
+    let testList = [];
+    const data = await getData("https://points.jshsus.kr/api2/points/view");
+
+    testList = data.map((x, idx) => {
+      const { stuid, grade, num, name, plus, minus } = x;
+      const className = x.class;
+
+      return [stuid, grade, className, num, name, plus, minus, 0, plus - minus];
     });
 
-    return response.data;
-}
+    setTableData(testList);
+    setColumns([
+      { data: "번호" },
+      { data: "일자" },
+      { data: "학년" },
+      { data: "반" },
+      { data: "번호" },
+      { data: "성명 (학번)" },
+      { data: "발급할 상점" },
+      { data: "발급할 벌점" },
+      { data: "처리 후 합계" },
+      { data: "사유코드" },
+      { data: "사유" },
+    ]);
+  }
 
-function Points_Apply(){
-    const [ columns, setColumns ] = useState([]);
-    const [ tableData, setTableData ] = useState([]);
+  return (
+    <>
+      <div id="points_apply">
+        <Card>
+          <Card.Header>
+            <Card.Title>상벌점 부여</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text className="label">학생 정보 입력</Card.Text>
 
-    useEffect(() => {
-        init();
-    }, []);
+            <Form onSubmit={handleSubmit}>
+              {/* Select 박스 (상단에 위치) */}
+              <Form.Group className="mb-3">
+                <Form.Label>선택</Form.Label>
+                <Form.Select
+                  name="selectedOption"
+                  value={inputs.selectedOption}
+                  onChange={handleSelect}
+                >
+                  <option value="">학번 찾기</option>
+                  {/* <option value="option1">옵션 1</option> */}
+                </Form.Select>
+              </Form.Group>
 
-    async function init(){
-        let testList = [];
+              {/* 학년 입력 */}
+              <Row className="g-2">
+                <Col md>
+                  <InputGroup>
+                    <InputGroup.Text className="bg-light text-dark">
+                      학년
+                    </InputGroup.Text>{" "}
+                    {/* 옅은 회색 배경으로 수정 */}
+                    <Form.Control
+                      type="number"
+                      name="grade"
+                      value={inputs.grade}
+                      onChange={handleChange}
+                      placeholder="학년"
+                      min="1"
+                      max="3"
+                    />
+                  </InputGroup>
+                </Col>
 
-        setTableData(testList);
-        setColumns([
-            { data: "ID" },
-            { data: "학년" },
-            { data: "반" },
-            { data: "번호" },
-            { data: "기준일자" },
-            { data: "성명 (학번)" },
-            { data: "유형" },
-            { data: "반영 내용" },
-            { data: "처리 후 합계" },
-            { data: "사유" },
-            { data: "반영일시" }
-        ]);
-    }
+                {/* 반 입력 */}
+                <Col md>
+                  <InputGroup>
+                    <InputGroup.Text className="bg-light text-dark">
+                      반
+                    </InputGroup.Text>{" "}
+                    {/* 옅은 회색 배경으로 수정 */}
+                    <Form.Control
+                      type="number"
+                      name="classNum"
+                      value={inputs.classNum}
+                      onChange={handleChange}
+                      placeholder="반"
+                      min="1"
+                      max="4"
+                    />
+                  </InputGroup>
+                </Col>
 
-    return(
-        <>
-        <div id="points_apply">
-            <Card>
-                <Card.Header><Card.Title>상벌점 부여</Card.Title></Card.Header>
-                <Card.Body>
-                    <Card.Text className="label">학생 검색</Card.Text>
-                    <Card.Text className="label">발급 내용</Card.Text>
-                    <Card.Text className="label">임의 표시 정보</Card.Text>
+                {/* 번호 입력 */}
+                <Col md>
+                  <InputGroup>
+                    <InputGroup.Text className="bg-light text-dark">
+                      번호
+                    </InputGroup.Text>{" "}
+                    <Form.Control
+                      type="number"
+                      name="studentNum"
+                      value={inputs.studentNum}
+                      onChange={handleChange}
+                      placeholder="번호"
+                      min="1"
+                      max="21"
+                    />
+                  </InputGroup>
+                </Col>
+              </Row>
+            </Form>
 
-                    <div className="tableWrap">
-                        <br/>
-                        <Card.Text className="label">발급할 상벌점 목록</Card.Text>
-                        <DataTable 
-                            className="pointsApplyTable"
-                            data={tableData}
-                            columns={columns}
-                        />
-                    </div>
-                </Card.Body>
-            </Card>
-        </div>
-        </>
-    )
+            <Card.Text className="label">발급 내용 입력</Card.Text>
+            <Row className="g-2">
+              {/* 사유 선택 */}
+              <Col md={6}>
+                <InputGroup>
+                  <InputGroup.Text className="bg-light text-dark">
+                    기준 규정
+                  </InputGroup.Text>
+                  <Form.Select
+                    name="reason"
+                    value={inputs.reason}
+                    onChange={handleChange}
+                  >
+                    <option value="">사유 선택</option>
+                    <option value="reason1">사유 1</option>
+                    <option value="reason2">사유 2</option>
+                    {/* 추가 사유 옵션 */}
+                  </Form.Select>
+                </InputGroup>
+              </Col>
+
+              {/* 상점 입력 */}
+              <Col md={3}>
+                <InputGroup>
+                  <InputGroup.Text className="bg-light text-dark">
+                    상점
+                  </InputGroup.Text>
+                  <Form.Control
+                    type="number"
+                    name="plusPoints"
+                    value={inputs.plusPoints}
+                    onChange={handleChange}
+                    min="0"
+                  />
+                </InputGroup>
+              </Col>
+
+              {/* 벌점 입력 */}
+              <Col md={3}>
+                <InputGroup>
+                  <InputGroup.Text className="bg-light text-dark">
+                    벌점
+                  </InputGroup.Text>
+                  <Form.Control
+                    type="number"
+                    name="minusPoints"
+                    value={inputs.minusPoints}
+                    onChange={handleChange}
+                    min="0"
+                  />
+                </InputGroup>
+              </Col>
+            </Row>
+            <Card.Text className="label">임의 표시 정보</Card.Text>
+            <Row className="g-2">
+              {/* 기준 일자 */}
+              <Col md={4}>
+                <InputGroup>
+                  <InputGroup.Text className="bg-light text-dark">
+                    기준 일자
+                  </InputGroup.Text>
+                  <Form.Control
+                    type="date"
+                    name="act_date"
+                    value={inputs.act_date || ""}
+                    onChange={handleChange}
+                  />
+                </InputGroup>
+              </Col>
+
+              {/* 부여 사유 */}
+              <Col md={6}>
+                <InputGroup>
+                  <InputGroup.Text className="bg-light text-dark">
+                    부여 사유
+                  </InputGroup.Text>
+                  <Form.Control
+                    type="text"
+                    name="reasonCaption"
+                    value={inputs.reasonCaption || ""}
+                    onChange={handleChange}
+                    placeholder="부여 사유 입력"
+                  />
+                </InputGroup>
+              </Col>
+            </Row>
+            {/* 제출 버튼 */}
+            <Button
+              className="mt-3"
+              variant="primary"
+              type="button"
+              onClick={handleClick}
+            >
+              추가
+            </Button>
+            <div className="tableWrap">
+              <br />
+              <Card.Text className="label">발급할 상벌점 목록</Card.Text>
+              <DataTable
+                className="pointsApplyTable"
+                data={tableData}
+                columns={columns}
+              />
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
 }
 
 export default Points_Apply;
--- src/pages/points/view/index.jsx
+++ src/pages/points/view/index.jsx
@@ -1,98 +1,93 @@
-import { useLocation, useNavigate, NavLink } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useLocation, useNavigate, NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-import axios from "axios";
-
-import './index.scss';
+import "./index.scss";
 
 import { Card, Button } from "react-bootstrap";
 import DataTable from "~shared/ui/datatable";
 
-const TITLE = import.meta.env.VITE_TITLE;
+import getData from "~shared/scripts/getData";
 
-async function getData(url, params = {}){
-    const response = await axios.get(`${url}`, {
-        params: params
+function Points_View() {
+  const [tableData, setTableData] = useState([]);
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  async function init() {
+    let testList = [];
+    const data = await getData("https://points.jshsus.kr/api2/points/view");
+    testList = data.map((x, idx) => {
+      const { stuid, grade, num, name, plus, minus } = x;
+      const className = x.class;
+
+      return [
+        <NavLink to="/check_history" key={idx}>
+          {stuid}
+        </NavLink>,
+        stuid,
+        grade,
+        className,
+        num,
+        <NavLink to="/check_history" key={idx}>
+          {name}
+        </NavLink>,
+        name,
+        plus,
+        minus,
+        0,
+        plus - minus,
+      ];
     });
 
-    return response.data;
-}
+    setTableData(testList);
+    setColumns([
+      { data: "학번", className: "dt-link", orderBase: 1 },
+      { hidden: true },
+      { data: "학년" },
+      { data: "반" },
+      { data: "번호" },
+      { data: "성명", className: "dt-name dt-link", orderBase: 6 },
+      { hidden: true },
+      { data: "누계 상점" },
+      { data: "누계 벌점" },
+      { data: "기타" },
+      { data: "합계", className: "dt-sum" },
+    ]);
+  }
 
-function Points_View(){
-    const [ tableData, setTableData ] = useState([]);
-    const [ columns, setColumns ] = useState([]);
+  return (
+    <>
+      <div id="points_view">
+        <Card>
+          <Card.Header>
+            <Card.Title>상벌점 현황</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text className="label">학생 검색</Card.Text>
 
-    useEffect(() => {
-        init();
-    }, []);
+            <div className="tableWrap">
+              <br />
+              <Card.Text className="label">상벌점 현황</Card.Text>
 
-    async function init(){
-        let testList = [];
-        const data = await getData("https://points.jshsus.kr/api/points/view");
-
-        testList = data.map((x, idx) => {
-            const { stuid, grade, num, name, plus, minus } = x;
-            const className = x.class;
-
-            return [
-                <NavLink to="/check_history">{stuid}</NavLink>,
-                stuid,
-                grade,
-                className,
-                num,
-                <NavLink to="/check_history">{name}</NavLink>,
-                name,
-                plus,
-                minus,
-                0,
-                plus - minus
-            ];
-        });
-
-        setTableData(testList);
-        setColumns([
-            { data: "학번", className: "dt-link", orderBase: 1 },
-            { hidden: true },
-            { data: "학년" },
-            { data: "반" },
-            { data: "번호" },
-            { data: "성명", className: "dt-name dt-link", orderBase: 6 },
-            { hidden: true },
-            { data: "누계 상점" },
-            { data: "누계 벌점" },
-            { data: "기타" },
-            { data: "합계", className: "dt-sum" }
-        ]);
-    }
-
-    return(
-        <>
-        <div id="points_view">
-            <Card>
-                <Card.Header><Card.Title>상벌점 현황</Card.Title></Card.Header>
-                <Card.Body>
-                    <Card.Text className="label">학생 검색</Card.Text>
-
-                    <div className="tableWrap">
-                        <br/>
-                        <Card.Text className="label">상벌점 현황</Card.Text>
-
-                        <DataTable
-                            className="pointsViewTable"
-                            columns={columns}
-                            data={tableData}
-                            options={{
-                                language: {
-                                    search: "통합 검색: "
-                                }
-                            }}
-                        />
-                    </div>
-                </Card.Body>
-            </Card>
-        </div>
-        </>
-    )
+              <DataTable
+                className="pointsViewTable"
+                columns={columns}
+                data={tableData}
+                options={{
+                  language: {
+                    search: "통합 검색: ",
+                  },
+                }}
+              />
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
 }
 
 export default Points_View;
