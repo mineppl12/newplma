a65d8bf3295861835966ad254ccca7dc5e5407fd
mineppl12 <mineppl12@gmail.com>
2025-03-21 18:01:51 +0900

    proxy

--- src/shared/scripts/getData.js
+++ src/shared/scripts/getData.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 async function getData(url, params = {}) {
   url = url.replace(/^\/api/, import.meta.env.VITE_API_BASE);
+  console.log(url);
 
   const response = await axios.get(url, {
     params,
