21e0bca84781b782ca0e84eecb7f807b346dcb28
mineppl12 <mineppl12@gmail.com>
2025-03-21 18:10:13 +0900

    proxy

--- src/pages/points/history/index.jsx
+++ src/pages/points/history/index.jsx
@@ -35,6 +35,7 @@ function Points_History() {
 
   async function init(allData = false) {
     const data = await getData('/api/points/history', { allData });
+    console.log(data);
 
     dataRef.current = data;
     setupTable(data);
