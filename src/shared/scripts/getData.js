a5647e60c6627698f072d4bfb6f9c3840299bd59
mineppl12 <mineppl12@gmail.com>
2025-03-22 20:53:42 +0900

    test

--- src/shared/scripts/getData.js
+++ src/shared/scripts/getData.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
 async function getData(url, params = {}) {
-    url = url.replace(
-        /^\/api/,
-        import.meta.env.MODE == 'production'
-            ? import.meta.env.VITE_PRODUCTION_API
-            : import.meta.env.VITE_API_BASE
-    );
+    url = url.replace(/^\/api/, import.meta.env.VITE_API_BASE);
 
     const response = await axios.get(url, {
         params,
